9569f8e31b4ac23ae4702179f766745bf00c56a3
Till Nikolas <till.n.hammer@outlook.com>
2021-09-26 16:43:36 +0200

    initialized git repo

---
+++ static/javascript/menu.js
@@ -0,0 +1,12 @@
+
+function showMenu() {
+
+        if (document.getElementById("menu").style.width = "30%") {
+                document.getElementById("menu").style.width = "0%";
+        }
+        if (document.getElementById("menu").style.width = "0%") {
+                document.getElementById("menu").style.width = "30%";
+        }
+}
+
+
