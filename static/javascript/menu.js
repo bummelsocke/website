2588b2724ca27cf01804995c566a481ab21a8c78
Till Nikolas <till.n.hammer@outlook.com>
2021-09-28 12:27:59 +0200

    relative scaling

--- static/javascript/menu.js
+++ static/javascript/menu.js
@@ -1,12 +1,7 @@
-
-function showMenu() {
-
-        if (document.getElementById("menu").style.width = "30%") {
-                document.getElementById("menu").style.width = "0%";
-        }
-        if (document.getElementById("menu").style.width = "0%") {
-                document.getElementById("menu").style.width = "30%";
-        }
+function openMenu() {
+        document.getElementById("popupMenu").style.width = "150px";
 }
 
-
+function closeMenu() {
+        document.getElementById("popupMenu").style.width = "0";
+}
